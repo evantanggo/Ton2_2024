be0d256537466ace44f4d0f278ba9e444d5eee39
Evan Tanggo <evan.tanggo25@gmail.com>
2023-11-13 18:47:25 +0100

    new file

--- Praxisaufgabe_2/PA2.py
+++ Praxisaufgabe_2/PA2.py
@@ -3,3 +3,37 @@ Praxisaufgabe 2
 Einzelwerte von Signalen
 '''
 
+import math
+from scipy.io.wavfile import read
+import numpy as np
+
+dataMono = np.array
+file = 'sinus_440Hz_mono.wav' #um file zu ändern Dateinamen anpassen
+(Fs, y) = read (file)
+
+if y.ndim == 2: #überprüft, ob die Datei Stereo oder Mono ist und rechnet sie in Mono um
+    y_L = y[:,0]
+    y_R = y[:,1]
+    dataMono = (y_L + y_R)/2
+else:
+    dataMono = y
+
+T = len(dataMono)
+samples = np.arange(len(dataMono)) #erstellt ein Array mit so viele Werten
+
+def effektivWert(daten):
+    global T, samples
+
+    daten = daten.astype(np.float64) #andert in float 64
+    square = np.square(daten)
+    integral = sum(square)  # summiert alle Werte des Arrays
+    eWert = math.sqrt(1 / T * integral)  # rechnet das Integral * 1/T und zieht die Wurzel
+    return eWert
+
+#def gleichRichtWert(daten):
+    #global T, samples
+
+
+
+print(effektivWert(dataMono))
+
